a3b667d99cb9d46860f1891842320794dfefdace
europa1610 <afzalahmad0203@gmail.com>
2021-03-11 20:14:29 +0800

    Initial Commit

---
+++ detection.py
@@ -0,0 +1,233 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import *
+from imutils import paths
+import cv2
+import numpy as np
+import torch.optim as optim
+from torch.autograd import Variable
+import os
+import argparse
+from torch.optim import lr_scheduler
+import wandb
+
+#Courtesy of https://github.com/detectRecog/CCPD/
+class ChaLocDataLoader(Dataset):
+    def __init__(self, img_dir, imgSize, is_transform=None):
+        self.img_dir = img_dir
+        self.img_paths = []
+        for i in range(len(img_dir)):
+            self.img_paths += [el for el in paths.list_images(img_dir[i])]
+        self.img_size = imgSize
+        self.is_transform = is_transform
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, index):
+        img_name = self.img_paths[index]
+        img = cv2.imread(img_name)
+        resizedImage = cv2.resize(img, self.img_size)
+        resizedImage = np.reshape(resizedImage, (resizedImage.shape[2], resizedImage.shape[0], resizedImage.shape[1]))
+
+        iname = img_name.rsplit('/', 1)[-1].rsplit('.', 1)[0].split('-')
+        [leftUp, rightDown] = [[int(eel) for eel in el.split('&')] for el in iname[2].split('_')]
+        #---Uncomment to generate image with bounding box
+        #cv2.rectangle(img, (leftUp[0], leftUp[1]), (rightDown[0], rightDown[1]), (255, 0, 0), 1) 
+        #cv2.imwrite('res1.jpg', img)
+        #---
+
+        ori_w, ori_h = float(img.shape[1]), float(img.shape[0])
+        assert img.shape[0] == 1160
+        new_labels = [(leftUp[0] + rightDown[0])/(2*ori_w), (leftUp[1] + rightDown[1])/(2*ori_h), (rightDown[0]-leftUp[0])/ori_w, (rightDown[1]-leftUp[1])/ori_h]
+
+        resizedImage = resizedImage.astype('float32')
+        resizedImage /= 255.0
+
+        return resizedImage, new_labels
+
+class wR2(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(wR2, self).__init__()
+        hidden1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=48, kernel_size=5, padding=2, stride=2),
+            nn.BatchNorm2d(num_features=48),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
+            nn.Dropout(0.2)
+        )
+        hidden2 = nn.Sequential(
+            nn.Conv2d(in_channels=48, out_channels=64, kernel_size=5, padding=2),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
+            nn.Dropout(0.2)
+        )
+        hidden3 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, padding=2),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
+            nn.Dropout(0.2)
+        )
+        hidden4 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=160, kernel_size=5, padding=2),
+            nn.BatchNorm2d(num_features=160),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
+            nn.Dropout(0.2)
+        )
+        hidden5 = nn.Sequential(
+            nn.Conv2d(in_channels=160, out_channels=192, kernel_size=5, padding=2),
+            nn.BatchNorm2d(num_features=192),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
+            nn.Dropout(0.2)
+        )
+        hidden6 = nn.Sequential(
+            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=5, padding=2),
+            nn.BatchNorm2d(num_features=192),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
+            nn.Dropout(0.2)
+        )
+        hidden7 = nn.Sequential(
+            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=5, padding=2),
+            nn.BatchNorm2d(num_features=192),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
+            nn.Dropout(0.2)
+        )
+        hidden8 = nn.Sequential(
+            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=5, padding=2),
+            nn.BatchNorm2d(num_features=192),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
+            nn.Dropout(0.2)
+        )
+        hidden9 = nn.Sequential(
+            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=192),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
+            nn.Dropout(0.2)
+        )
+        hidden10 = nn.Sequential(
+            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=192),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
+            nn.Dropout(0.2)
+        )
+        self.features = nn.Sequential(
+            hidden1,
+            hidden2,
+            hidden3,
+            hidden4,
+            hidden5,
+            hidden6,
+            hidden7,
+            hidden8,
+            hidden9,
+            hidden10
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(23232, 100),
+            # nn.ReLU(inplace=True),
+            nn.Linear(100, 100),
+            # nn.ReLU(inplace=True),
+            nn.Linear(100, num_classes),
+        )
+
+    def forward(self, x):
+        x1 = self.features(x)
+        x11 = x1.view(x1.size(0), -1)
+        x = self.classifier(x11)
+        return x
+
+def draw_bbox(image, bbox):
+    h, w = image.shape[1], image.shape[2]
+    image = np.reshape(image, (image.shape[1],image.shape[2], image.shape[0]))
+    topleft_x = int(bbox[0]*w - bbox[2]*w/2.)
+    topleft_y = int(bbox[1]*h - bbox[3]*h/2.)
+    botright_x = int(bbox[0]*w + bbox[2]*w/2.)
+    botright_y = int(bbox[1]*h + bbox[3]*h/2.)
+    cv2.rectangle(image, (topleft_x, topleft_y), (botright_x, botright_y), (255, 0, 0), 1) 
+    cv2.imwrite('res.jpg', image*255.0)
+
+def train_model(model, criterion, optimizer, num_epochs=25):
+    # since = time.time()
+    for epoch in range(epoch_start, num_epochs):
+        lossAver = []
+        model.train(True)
+        lrScheduler.step()
+        start = time()
+
+        for i, (XI, YI) in enumerate(trainloader):
+            # print('%s/%s %s' % (i, times, time()-start))
+            YI = np.array([el.numpy() for el in YI]).T
+            if use_gpu:
+                x = Variable(XI.cuda(0))
+                y = Variable(torch.FloatTensor(YI).cuda(0), requires_grad=False)
+            else:
+                x = Variable(XI)
+                y = Variable(torch.FloatTensor(YI), requires_grad=False)
+            # Forward pass: Compute predicted y by passing x to the model
+            y_pred = model(x)
+
+            # Compute and print loss
+            loss = 0.0
+            if len(y_pred) == batchSize:
+                loss += 0.8 * nn.L1Loss().cuda()(y_pred[:][:2], y[:][:2])
+                loss += 0.2 * nn.L1Loss().cuda()(y_pred[:][2:], y[:][2:])
+                lossAver.append(loss.data[0])
+
+                # Zero gradients, perform a backward pass, and update the weights.
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                torch.save(model.state_dict(), storeName)
+            if i % 50 == 1:
+                with open(args['writeFile'], 'a') as outF:
+                    outF.write('train %s images, use %s seconds, loss %s\n' % (i*batchSize, time() - start, sum(lossAver[-50:]) / len(lossAver[-50:])))
+        print ('%s %s %s\n' % (epoch, sum(lossAver) / len(lossAver), time()-start))
+        with open(args['writeFile'], 'a') as outF:
+            outF.write('Epoch: %s %s %s\n' % (epoch, sum(lossAver) / len(lossAver), time()-start))
+        torch.save(model.state_dict(), storeName + str(epoch))
+    return model
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--images", required=True,
+                help="path to the input file")
+ap.add_argument("-n", "--epochs", default=25,
+                help="epochs for train")
+ap.add_argument("-b", "--batchsize", default=4,
+                help="batch size for train")
+ap.add_argument("-r", "--resume", default='111',
+                help="file for re-train")
+ap.add_argument("-w", "--writeFile", default='wR2.out',
+                help="file for output")
+args = vars(ap.parse_args())
+
+def main():
+    numClasses = 4
+    img_size = (480, 480)
+    batchSize = args['batchsize']
+    epochs = args['epochs']
+    model = wR2(numClasses)
+    model = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
+    model = model.cuda()
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    lrScheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+    dst = ChaLocDataLoader(args["images"].split(','), imgSize)
+    trainloader = DataLoader(dst, batch_size=batchSize, shuffle=True, num_workers=4)
+    model_conv = train_model(model, criterion, optimizer, num_epochs=epochs)
+
+if __name__=='__main__':
+    main()
+
+#image_loc = ['/media/largeHDD/afzal/dl_project/placeholder_data/']
+#loader = ChaLocDataLoader(image_loc, img_size)
+#item, label = loader.__getitem__(0)
+#draw_bbox(item, label)
